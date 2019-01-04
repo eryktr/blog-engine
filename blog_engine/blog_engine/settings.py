4adbb0ca61889858fedaee7df205fb40752879ff
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-04 21:09:40 +0100

    Added basic layout files and static files

--- blog_engine/blog_engine/settings.py
+++ blog_engine/blog_engine/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blog_engine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/base')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
