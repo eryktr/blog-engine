a48dabe460a75fb11daaab9679709675c2652ad2
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-07 23:50:03 +0100

    Authorized users can now edit posts

--- blog_engine/content/urls.py
+++ blog_engine/content/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("new_post/", views.new_post, name="newpost"),
     path("delete_post/<int:post_id>", views.delete_post, name="delete_post"),
-    path("view_post/<int:post_id>", views.view_post, name="view_post")
+    path("view_post/<int:post_id>", views.view_post, name="view_post"),
+    path("edit_post/<int:post_id>", views.edit_post, name="edit_post")
 ]
